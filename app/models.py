de20b7186d7732394612402ddd94424cd19128bf
caiseyann <caiseyann4@gmail.com>
2020-09-21 14:45:15 +0300

    defined new function user

--- app/models.py
+++ app/models.py
@@ -9,3 +9,21 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(5000))
+    profile_pic_path = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+
+    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
+
+    comments = db.relationship('Comment', backref='user', lazy="dynamic")
