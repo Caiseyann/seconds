d827989bf891c3f42baa3c8d53792db02a533d6d
caiseyann <caiseyann4@gmail.com>
2020-09-21 10:19:37 +0300

    defininf modules

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
-from app import create_app,db
-from app.models import User,Role
-from flask_script import Manager,Server
+from app import create_app, db
+from app.models import User, Role
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 app = create_app('test')
+
+manager = Manager(app)
+manager.add_command('server', Server)
 
